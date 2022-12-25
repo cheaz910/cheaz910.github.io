e69cf0d60a55c38e6d01367f61c0039671534a50
Ilnur <cheaz910@mail.ru>
2022-12-25 23:21:46 +0500

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState, useCallback, useEffect } from 'react';
+
 import './App.css';
+import Question from './components/Question/Question';
+import data from './questions.json';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [questions, setQuestions] = useState([]);
+  const onAnswer = useCallback((isTrue) => setCount(prev => prev + +isTrue), []);
+
+  useEffect(() => {
+    setQuestions(data.questions.sort(() => Math.random() - 0.5).map(x => ({...x, answers: x.answers.sort(() => Math.random() - 0.5)})))
+  }, [data]);
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {questions.map((x, index) => (
+        <div key={x.question} className="QuestionContainer">
+          <Question onAnswer={onAnswer} id={index} question={x.question} answers={x.answers} trueAnswer={x.trueAnswer} />
+        </div>
+      ))}
+      <div className="Counter">{`Правильно ${count} из ${data.questions.length}`}</div>
     </div>
   );
 }
---
+++ src/components/Question/Question.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, memo } from 'react';
+
+import './styles.css';
+
+function Question({onAnswer, id, question, answers, trueAnswer}) {
+  const [selected, setSelected] = useState('');
+
+  return (
+    <div className="Question">
+      <div className="Title">{question}</div>
+      <div className="Answers">
+        {answers.map((answer, index) => (
+          <div key={index} className={`Answer${answer === selected && selected === trueAnswer ? ' Answer__true' : (selected !== '' && answer === selected ? ' Answer__false' : '')}`}>
+            <input 
+              disabled={selected !== ''} 
+              onChange={() => {
+                setSelected(answer);
+                if (answer === trueAnswer) {
+                  onAnswer(true);
+                } else {
+                  onAnswer(false);
+                }
+              }} 
+              id={`${id}-answer-${index}`} 
+              name={`${id}-answer`} 
+              type="radio" 
+            />
+            <label htmlFor={`${id}-answer-${index}`}>{answer}</label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default memo(Question);
